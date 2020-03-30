ed6a1883e4a26a45605aaf4d7766c72679dd99b3
Florian Schlembach <1262767+floschl@users.noreply.github.com>
2020-03-30 07:39:31 +0200

    retrigger build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ set MSSdk=1
 
 if %PY_VER%==3.5 (
 REM This one is essential for getting DLL linkage on Py3.5+
-    set "PY_VCRUNTIME_REDIST=%LIBARARY_BIN%\vcruntime140.dll"
+    set  "PY_VCRUNTIME_REDIST=%LIBARARY_BIN%\vcruntime140.dll"
     set VS=vs2015
 ) else (
     set VS=vs2010
